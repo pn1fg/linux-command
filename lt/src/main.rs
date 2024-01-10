1f6fb46e3c6e08ee34f51705bb133ff98aded4f7
pn1fg <118673148+pn1fg@users.noreply.github.com>
2024-01-10 17:19:09 +0800

    :sparkles: 添加列出指定路径文件目录并且支持列出所有文件目录选项

--- lt/src/main.rs
+++ lt/src/main.rs
@@ -1,18 +1,29 @@
+use clap::Parser;
 use std::{fs, io};
 
-fn main() -> io::Result<()> {
-    let dir = ".";
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
 
-    let mut files = fs::read_dir(dir)?
+struct Args {
+    #[clap(default_value = ".")]
+    name: String,
+    #[arg(short = 'a')]
+    all: bool,
+}
+
+fn main() -> io::Result<()> {
+    let args = Args::parse();
+
+    let mut files = fs::read_dir(&args.name)?
         .filter_map(Result::ok)
         .map(|entry| entry.path())
         .filter(|path| {
             path.file_name()
                 .and_then(|name| name.to_str())
-                .map_or(false, |name| !name.starts_with('.'))
+                .map_or(false, |name| !name.starts_with('.') || args.all)
         })
         .filter_map(|path| {
-            let path_str = path.strip_prefix(dir).unwrap_or(&path).to_str()?;
+            let path_str = path.strip_prefix(&args.name).unwrap_or(&path).to_str()?;
             let mut display_str = path_str.to_string();
             if path.is_dir() {
                 display_str.push('/');
